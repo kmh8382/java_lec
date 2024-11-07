883c847e62faef87282826bee46873b918a4b85f
kmh8382 <gorxhfm82@naver.com>
2024-11-07 17:19:05 +0900

    20241107

---
+++ 06_class/src/pkg07_access_modifier/AccessModifierEx.java
@@ -0,0 +1,15 @@
+package pkg07_access_modifier;
+
+public class AccessModifierEx {
+
+  public static void main(String[] args) {
+    
+    // Sample 타입의 인스턴스 s 생성
+    Smaple s = new Smaple();
+    
+    // s.item = "데이터";
+    s.setItem("데이터");
+
+  }
+
+}
--- 06_class/src/pkg09_Member/MemberEx.java
+++ 06_class/src/pkg09_Member/MemberEx.java
@@ -7,10 +7,10 @@ public class MemberEx {
     // Member 인스턴스 m 생성
 //    Address address = new Address("12345", "서울시 강남구 테헤란로", "장연빌딩 5층");
 //    Contact contact = new Contact("010-1234-5678");
-//    Member m = new Member("name", address, contact);
+//    Member m = new Member("홍길동", address, contact);
     Member m = new Member("홍길동", 
-                               new Address("12345", "서울시 강남구 테헤란로", "장연빌딩 5층"), 
-                               new Contact("010-1234-5678"));
+                          new Address("12345", "서울시 강남구 테헤란로", "장연빌딩 5층"), 
+                          new Contact("010-1234-5678"));
     m.info();
     
 
